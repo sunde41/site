b9cab25f7accac6146065cfcb4e17f6a985dd589
sunde41 <sunde41@gmail.com>
2019-02-14 19:26:03 +0900

    third commit

--- judge/views/problem.py
+++ judge/views/problem.py
@@ -154,7 +154,7 @@ class ProblemRaw(ProblemMixin, TitleMixin, TemplateResponseMixin, SingleObjectMi
             ))
 
 
-class ProblemDetail(ProblemMixin, SolvedProblemMixin):
+class ProblemDetail(ProblemMixin, SolvedProblemMixin, View):
     context_object_name = 'problem'
     template_name = 'problem/problem.html'
 
