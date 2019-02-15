0168f84b881788c32bf6d1a66bbd6edc5d147532
sunde41 <sunde41@gmail.com>
2019-02-15 10:21:28 +0900

    third commit

--- judge/admin/interface.py
+++ judge/admin/interface.py
@@ -58,7 +58,6 @@ class NoticePostAdmin(VersionAdmin):
     fieldsets = (
         (None, {'fields': ('title', 'slug', 'authors', 'visible', 'sticky', 'publish_on')}),
         (_('Content'), {'fields': ('content', )}),
-        ({'classes': ('collapse',), }),
     )
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('id', 'title', 'visible', 'sticky', 'publish_on')
