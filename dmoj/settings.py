fde9fa0f9e958085ee5d2444e1e96701dee7fda6
sunde41 <sunde41@gmail.com>
2019-02-19 23:39:18 +0900

    1차 완료

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -1,14 +1,5 @@
-"""
-Django settings for dmoj project.
+# -*- coding: utf-8 -*-
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.11/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import re
 
