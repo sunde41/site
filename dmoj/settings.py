bd9873dee6f92b1de1bfee301c83841f53e64419
sunde41 <sunde41@gmail.com>
2019-02-20 11:32:59 +0900

    1차 완료

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -23,6 +23,8 @@ ADMINS = (
 )
 
 TERMS_OF_SERVICE_URL = 'http://korea.ac.kr/mbshome/mbs/university/subview.do?id=university_090300000000'  # Use a flatpage.
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 
 PYGMENT_THEME = 'pygment-github.css'
 PROBLEM_DATA_ROOT = '/root/problems'
