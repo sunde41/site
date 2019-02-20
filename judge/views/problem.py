d8ce4000377e39f8bc1b1338fc188b151a747c3f
sunde41 <sunde41@gmail.com>
2019-02-20 15:49:50 +0900

    1차 완료

--- judge/views/problem.py
+++ judge/views/problem.py
@@ -165,6 +165,7 @@ class ProblemDetail(LoginRequiredMixin, ProblemMixin, SolvedProblemMixin, Detail
         authed = user.is_authenticated
         context['has_submissions'] = authed and Submission.objects.filter(user=user.profile,
                                                                           problem=self.object).exists()
+        context['has_solved_problem'] = self.object.id in self.get_completed_problems()
         contest_problem = (None if not authed or user.profile.current_contest is None else
                            get_contest_problem(self.object, user.profile))
         context['contest_problem'] = contest_problem
