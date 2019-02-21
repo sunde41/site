e7a026180222f7598ae36d96a51570c181ba1dce
sunde41 <sunde41@gmail.com>
2019-02-22 00:55:14 +0900

    1차 완료

--- static/common.js
+++ static/common.js
@@ -204,10 +204,11 @@ function count_down(label) {
         var m = Math.floor(time % 3600 / 60);
         var s = time % 60;
         if (d > 0)
-            label.text(npgettext('time format with day', '%d day %h:%m:%s', '%d days %h:%m:%s', d)
+            label.text(npgettext('time format with day', '%d일 %h시 %m분 %s초', '%d일 %h시 %m분 %s초', d)
                 .replace('%d', d).replace('%h', format(h)).replace('%m', format(m)).replace('%s', format(s)));
         else
-            label.text(pgettext('time format without day', '%h시 %m분 %s초')
+            label.text(pgettext('time format without day', '%h시 %m분 %s초' +
+                '')
                 .replace('%h', format(h)).replace('%m', format(m)).replace('%s', format(s)));
     }, 1000);
 }
